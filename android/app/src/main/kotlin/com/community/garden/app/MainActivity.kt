85d484c2b093e4d8cb3bd42c5a217f26ada3b33c
Dogbone0714 <jameskang0714@gmail.com>
2025-06-20 23:03:43 +0800

    測試沒問題

---
+++ android/app/src/main/kotlin/com/community/garden/app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.community.garden.app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity() 
--- android/app/src/main/kotlin/com/example/startnest/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.startnest
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
